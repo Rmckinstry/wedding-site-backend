4bc2f01f53b79c3bfe9d3069c0095db2b27b4eb3
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-14 16:36:20 -0600

    adding GET routes and controller methods

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,3 +13,28 @@ export const getAllUsers = async (req, res)=>{
         res.status(500).send('Internal Server Error');
     }
 }
+
+export const getUserById = async (req, res) =>{
+    try {
+        const {userId} = req.params;
+        const result = await db.query(`SELECT * FROM ${tableName} WHERE user_id = $1`, [userId])
+
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+export const getUserByGroup = async (req, res) => {
+    try {
+        const {groupId} = req.params;
+        const result = await db.query(`SELECT * FROM ${tableName} WHERE group_id = $1`, [groupId])
+
+        res.json(result.rows);
+        
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+}
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getAllUsers } from '../controllers/userController.js';
+import { getAllUsers, getUserById, getUserByGroup } from '../controllers/userController.js';
 
 
 const router = express.Router();
 
 // GET
 router.get('/', getAllUsers);
+router.get('/:userId', getUserById);
+router.get('/group/:groupId', getUserByGroup);
 
 export default router;
