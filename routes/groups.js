c69fc66eb85955166e1cc8fdf40c6c8a9ff90806
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-14 15:00:01 -0600

    route tweaks

--- routes/groups.js
+++ routes/groups.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.get('/', getAllGroups);
 router.get('/:id', getGroup);
 router.post('/', addGroup);
-router.put('/', editGroupName);
-router.delete('/', deleteGroup);
+router.put('/:id', editGroupName);
+router.delete('/:id', deleteGroup);
 
 export default router;
