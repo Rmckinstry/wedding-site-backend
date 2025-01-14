66958c4f08eb4d912ecfd952066b1e029f0153ed
Rmckinstry <ryanmckinstry2@gmail.com>
2025-01-14 14:07:09 -0600

    starting index route

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import db from './utils/db.js';
+import routes from './routes/index.js'
+
 const app = express();
 const port = 3000;
 const tableName = "groups"
 
 app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// central router from routes/index.js
+app.use('/', routes);
+
 
 app.get('/', async (req, res)=>{
     try{
---
+++ routes/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import groupsRoute from './groups.js'
+
+const router = express.Router();
+
+router.use('/groups', groupsRoute);
+
+// catch-all route for undefined routes if needed
+router.use((req, res, next) => {
+    res.status(404).send("Sorry, we couldn't find what you're looking for!");
+});
