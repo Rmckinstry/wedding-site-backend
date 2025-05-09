b8e56e3e0d16abbeaf7d1400fc8d917bb701e1ad
Rmckinstry <ryanmckinstry2@gmail.com>
2025-05-08 23:31:13 -0500

    adding logic for creating additional guest.

--- routes/guests.js
+++ routes/guests.js
@@ -1,25 +1,35 @@
-import express from 'express';
-import { getAllGuests, getGuestById, getGuestByGroup, createGuest, editGuest, editHasDependent, editPlusOneAllowed, deleteGuest } from '../controllers/guestController.js';
-
+import express from "express";
+import {
+    getAllGuests,
+    getGuestById,
+    getGuestByGroup,
+    createGuest,
+    createAdditional,
+    editGuest,
+    editHasDependent,
+    editPlusOneAllowed,
+    deleteGuest,
+} from "../controllers/guestController.js";
 
 const router = express.Router();
 
 // GET
-router.get('/', getAllGuests);
-router.get('/:guestId', getGuestById);
-router.get('/group/:groupId', getGuestByGroup);
+router.get("/", getAllGuests);
+router.get("/:guestId", getGuestById);
+router.get("/group/:groupId", getGuestByGroup);
 
 // POST
-router.post('', createGuest);
+router.post("", createGuest);
+router.post("/additional", createAdditional);
 
 // PUT
-router.put('/:guestId', editGuest);
+router.put("/:guestId", editGuest);
 
 // PATCH
-router.patch('/dependent/:guestId', editHasDependent);
-router.patch('/plus-one/:guestId', editPlusOneAllowed);
+router.patch("/dependent/:guestId", editHasDependent);
+router.patch("/plus-one/:guestId", editPlusOneAllowed);
 
 // DELETE
-router.delete('/:guestId', deleteGuest);
+router.delete("/:guestId", deleteGuest);
 
 export default router;
