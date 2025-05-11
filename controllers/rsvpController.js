ca9464ca676c21851b312bde4c458120e555a821
Rmckinstry <ryanmckinstry2@gmail.com>
2025-05-11 01:05:51 -0500

    adding create RSVP

--- controllers/rsvpController.js
+++ controllers/rsvpController.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import db from '../utils/db';
+import db from '../utils/db.js';
 
 const router = express.Router();
 const tableName = "rsvps";
@@ -9,6 +9,59 @@ export const getAllRSVPs = async (req, res) => {
         const result = await db.query(`SELECT * FROM ${tableName}`);
         res.json(result.rows);
     } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+export const getRSVP = async (req, res) => {
+    try {
+        const { rsvpId } = req.params;
+        const result = await db.query(`SELECT * FROM ${tableName} WHERE rsvp_id = $1`, [rsvpId]);
+
+        res.json(result.rows);
+    } catch (error) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+export const createRSVP = async (req, res) => {
+    try {
+        const { guestId, attendance, spotify } = req.body;
+        const timestamp = new Date(Date.now()).toISOString()
+        console.log(timestamp)
+
+        const result = await db.query(
+            `INSERT INTO ${tableName} (guest_id, attendance, spotify, created_at)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *`,
+            [guestId, attendance, spotify, timestamp]
+        );
+
+        res.status(201).json({
+            message: "RSVP inserted successfully",
+            data: result.rows,
+        });
+    } catch (error) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
+    }
+}
+
+export const deleteRSVP = async (req, res) => {
+    try {
+        const { rsvpId } = req.params;
+
+        const result = await db.query(`DELETE FROM ${tableName} WHERE rsvp_id = $1`, [rsvpId]);
+
+        if (result.rowCount === 0) {
+            return res.status(404).send('Record not found');
+        }
+
+        res.status(200).send('RSVP deleted successfully');
+
+    } catch (error) {
         console.error(err);
         res.status(500).send('Internal Server Error');
     }
--- routes/rsvps.js
+++ routes/rsvps.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { getAllRSVPs } from '../controllers/rsvpController';
+import { getAllRSVPs, createRSVP, getRSVP, deleteRSVP } from '../controllers/rsvpController.js';
 const router = express.Router();
 
 // GET
 router.get('/', getAllRSVPs);
+router.get('/:rsvpId', getRSVP);
 // POST
-
+router.post('/', createRSVP)
 // PUT
 
 // DELETE
-
+router.delete('/:rsvpId', deleteRSVP);
 export default router;
