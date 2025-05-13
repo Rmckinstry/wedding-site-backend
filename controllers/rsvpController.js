7bf6232e77b96d77e6bce391cab1077b251c3ca6
Rmckinstry <ryanmckinstry2@gmail.com>
2025-05-13 16:42:50 -0500

    refactoring controller logic to call rsvp service

--- controllers/rsvpController.js
+++ controllers/rsvpController.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import db from '../utils/db.js';
+import { createRSVPs } from '../services/rsvpService.js';
 
 const router = express.Router();
 const tableName = "rsvps";
@@ -28,20 +29,17 @@ export const getRSVP = async (req, res) => {
 
 export const createRSVP = async (req, res) => {
     try {
-        const { guestId, attendance, spotify } = req.body;
-        const timestamp = new Date(Date.now()).toISOString()
-        console.log(timestamp)
+        const { rsvpList } = req.body;
 
-        const result = await db.query(
-            `INSERT INTO ${tableName} (guest_id, attendance, spotify, created_at)
-            VALUES ($1, $2, $3, $4)
-            RETURNING *`,
-            [guestId, attendance, spotify, timestamp]
-        );
+        if (!Array.isArray(rsvpList) || rsvpList.length === 0) {
+            return res.status(400).send("Param must be a non empty array of rsvps");
+        }
+
+        const rsvps = await createRSVPs(rsvpList);
 
         res.status(201).json({
-            message: "RSVP inserted successfully",
-            data: result.rows,
+            message: "RSVP(s) inserted successfully",
+            data: rsvps,
         });
     } catch (error) {
         console.error(err);
