6b9d70929643d01798e6f978288bc6c23c2a5e67
Rmckinstry <ryanmckinstry2@gmail.com>
2025-05-16 00:54:52 -0500

    testing pool direct connection

--- utils/db.js
+++ utils/db.js
@@ -2,19 +2,19 @@ import pg from 'pg';
 const { Pool } = pg;
 
 // Used for local
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT || 5432,
-//     database: process.env.DB_NAME
-// })
-console.log(process.env.DB_URL);
 const pool = new Pool({
-    connectionString: process.env.DB_URL,
-    ssl: process.env.DB_URL ? true : false
-});
-console.log(pool);
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_NAME
+})
+// console.log(process.env.DB_URL);
+// const pool = new Pool({
+//     connectionString: process.env.DB_URL,
+//     ssl: process.env.DB_URL ? true : false
+// });
+// console.log(pool);
 
 export default {
     query: (text, params) => pool.query(text, params),
