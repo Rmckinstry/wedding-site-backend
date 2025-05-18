43ee26d975058d5ede84c15cdc0a2f63a35b8608
Rmckinstry <ryanmckinstry2@gmail.com>
2025-05-17 22:22:35 -0500

    removing uneeded db code

--- utils/db.js
+++ utils/db.js
@@ -7,14 +7,9 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    ssl: true
 })
-// console.log(process.env.DB_URL);
-// const pool = new Pool({
-//     connectionString: process.env.DB_URL,
-//     ssl: process.env.DB_URL ? true : false
-// });
-// console.log(pool);
 
 export default {
     query: (text, params) => pool.query(text, params),
