bd4060f4daf984c9e1ef5d5371d8767e4ca3692d
Rmckinstry <ryanmckinstry2@gmail.com>
2025-05-17 22:39:05 -0500

    adding env check for render

--- utils/db.js
+++ utils/db.js
@@ -1,14 +1,13 @@
 import pg from 'pg';
 const { Pool } = pg;
 
-// Used for local
 const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME,
-    ssl: true
+    ssl: process.env.ENV_TAG === 'local' ? true : false
 })
 
 export default {
