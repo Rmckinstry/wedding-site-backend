e7520fe615fb4fa96d6d539931974f9034f880d1
Rmckinstry <ryanmckinstry2@gmail.com>
2025-06-13 14:25:00 -0500

    updating port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import cors from 'cors';
 import routes from './routes/index.js'
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(cors());
 
@@ -23,5 +23,5 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 })
